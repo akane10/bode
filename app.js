41f004b6d5401712321eb5829db78a66629095ca
yapie <targaryen.akane@gmail.com>
2018-10-18 15:35:00 +0700

    separate config key from app.js

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const Twit = require('twit');
 const request = require('request');
-
-const key = require('./keys.js');
- 
-const T = new Twit({
-  consumer_key: key.consumer_key,
-  consumer_secret: key.consumer_secret,
-  access_token: key.access_token,
-  access_token_secret: key.access_token_secret,
-});
+const T = require('./config.js');
 
 const options = {
   url: 'https://catfact.ninja/fact?max_length=280',
@@ -20,9 +11,10 @@ function tweet() {
 		if (err) throw err;
 		const obj = JSON.parse(body);
 		T.post('statuses/update', { status: obj.fact }, function(err, data, response) {
+			if (err) return console.log(err);
 			console.log('done');
 		});
 	});
-}
+};
 
 setInterval(tweet, 28800000); //8 hours
